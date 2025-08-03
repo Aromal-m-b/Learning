572fdb0c4efce1714e019615d8426f54a4bcc426
AROMAL  M B <130879163+Aromal-m-b@users.noreply.github.com>
2025-08-03 16:35:24 +0530

    Update main.py

--- LangGraph/chatbot/main.py
+++ LangGraph/chatbot/main.py
@@ -1,1 +1,39 @@
 
+from typing import TypedDict,Annotated
+from langgraph.graph import StateGraph,END,START
+from langgraph.graph.message import add_messages as add
+from langchain_community.chat_models import ChatOllama
+
+class State(TypedDict):
+    messages:Annotated[list,add]
+
+llm = ChatOllama(model = "mistral:instruct")
+
+
+def chatbot(state:State):
+    return { 
+        'messages' : [llm.invoke(state['messages'])]
+    }
+
+builder = StateGraph(State)
+builder.add_node('llm_node',chatbot)
+builder.add_edge(START,'llm_node')
+builder.add_edge('llm_node',END)
+
+graph = builder.compile()
+
+state = None
+
+while True:
+    in_message =  input("Human:- ")
+    if in_message == "\\quit":
+        break
+    if state == None:
+        state:State = {
+            "messages":[{"role" : "user","content":in_message}]
+        }
+    else:
+        state["messages"].append({"role" : "user","content":in_message})
+
+    state = graph.invoke(state)
+    print("Bot :- ",state["messages"][-1].content)
